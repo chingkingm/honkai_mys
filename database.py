62107f634595c3ac5a46f2f664a9a347bb36f577
chingkingm <chingkingm@gmail.com>
2022-01-13 13:17:24 +0800

    add:空白消息处理

--- database.py
+++ database.py
@@ -10,15 +10,20 @@ class DB(SqliteDict):
     def __init__(self, filename=None, tablename='unnamed', flag='c', autocommit=True, journal_mode="DELETE", encode=json.dumps, decode=json.loads):
         filename = os.path.join(os.path.dirname(__file__),self.cahce_dir,filename)
         super().__init__(filename=filename, tablename=tablename, flag=flag, autocommit=autocommit, journal_mode=journal_mode, encode=encode, decode=decode)   
-    def set_region(self,qid,region):
-        if self.get(qid):
-            data = self[qid]
+    def set_region(self,role_id,region):
+        if self.get(role_id):
+            data = self[role_id]
             data.update({"region":region})
         else:
             data = {"region":region}
-        self[qid] = data
-    def get_region(self,qid) -> Optional[str]:
-        if self.get(qid):
-            return self[qid]["region"]
+        self[role_id] = data
+    def get_region(self,role_id) -> Optional[str]:
+        if self.get(role_id):
+            return self[role_id]["region"]
         else:
             return None
+    def get_uid_by_qid(self,qid:str):
+        """获取上次查询的uid"""
+        return self[qid]["role_id"]
+    def set_uid_by_qid(self,qid:str,uid:str):
+        self[qid] = {"role_id":uid}
