ee05572012cddcf0ded1a5bfac07ea23040ea3b6
chingkingm <chingkingm@gmail.com>
2022-02-12 11:48:04 +0800

    format

--- guess_voice/__init__.py
+++ guess_voice/__init__.py
@@ -1,93 +1,105 @@
-import random
-from hoshino import HoshinoBot, Service, MessageSegment, priv
-from hoshino.util import FreqLimiter
-from hoshino.typing import CQEvent
 import json
 import os
+import random
 import re
-from rich import print
-from loguru import logger
+
+from hoshino import HoshinoBot, MessageSegment, Service, priv
+from hoshino.typing import CQEvent
+from hoshino.util import FreqLimiter
+
 from .game import GameSession
+
 _help = """
 [崩坏3猜语音]：正常舰桥、战斗等语音
 [崩坏3猜语音2/困难]：简短的语气或拟声词
 """
-FN = 120
+FN = 30
 flmt = FreqLimiter(FN)
-sv = Service("崩坏3猜语音",bundle="崩坏3",help_=_help)
-def split_voice_by_chara(v_list:list):
+sv = Service("崩坏3猜语音", bundle="崩坏3", help_=_help)
+
+
+def split_voice_by_chara(v_list: list):
     """对语音列表进行分类"""
     ret = {
-        "normal":{},# 正常语音
-        "hard":{}   # 语气&拟声词
+        "normal": {},  # 正常语音
+        "hard": {}   # 语气&拟声词
     }
     for voice in v_list:
         op_dict = ret["hard"] if "拟声词" in voice["voice_path"] else ret["normal"]
-        chara = re.split('-|\(',voice['voice_name'])[0].strip()
-        if re.search(r"《|【",chara):
+        chara = re.split('-|\(', voice['voice_name'])[0].strip()
+        if re.search(r"《|【", chara):
             continue
         if not op_dict.get(chara):
             op_dict[chara] = []
         op_dict[chara].append(voice)
     return ret
+
+
 def gen_voice_list(origin_path=None):
     """递归生成语音列表"""
-    voice_dir = os.path.join(os.path.dirname(__file__),"../assets/record")
+    voice_dir = os.path.join(os.path.dirname(__file__), "../assets/record")
     if origin_path == None:
         origin_path = voice_dir
     ret_list = []
     for item in os.listdir(origin_path):
-        item_path = os.path.join(origin_path,item)
+        item_path = os.path.join(origin_path, item)
         if os.path.isdir(item_path):
             ret_list.extend(gen_voice_list(item_path))
         elif item.endswith("mp3"):
-            voice_path = os.path.relpath(item_path,voice_dir)
-            ret_list.append({"voice_name":item,"voice_path":voice_path})
+            voice_path = os.path.relpath(item_path, voice_dir)
+            ret_list.append({"voice_name": item, "voice_path": voice_path})
         else:
             continue
     return ret_list
 
-@sv.on_prefix(("崩坏3猜语音","崩坏猜语音","bh3猜语音","bh猜语音","崩3猜语音","崩坏三猜语音"))
-async def guess_voice(bot:HoshinoBot,ev:CQEvent):
+
+@sv.on_prefix(("崩坏3猜语音", "崩坏猜语音", "bh3猜语音", "bh猜语音", "崩3猜语音", "崩三猜语音", "崩坏三猜语音"))
+async def guess_voice(bot: HoshinoBot, ev: CQEvent):
     msg = str(ev.message.extract_plain_text().strip())
-    if re.search(r"2|困难",msg):
+    if re.search(r"2|困难", msg):
         difficulty = "hard"
     else:
         difficulty = "normal"
     game = GameSession(ev.group_id)
     ret = await game.start(difficulty=difficulty)
-    await bot.send(ev,ret)
+    await bot.send(ev, ret)
+
+
 @sv.on_message()
-async def check_answer(bot,ev:CQEvent):
+async def check_answer(bot, ev: CQEvent):
     msg = ev.message.extract_plain_text().strip()
-    msg = msg.lower().replace(",","和").replace("，","和")
+    msg = msg.lower().replace(",", "和").replace("，", "和")
     game = GameSession(ev.group_id)
     if not game.is_start:
         return
-    await game.check_answer(msg,ev.user_id)
-@sv.on_prefix(("崩坏3语音","崩坏语音","bh3语音","bh语音","崩3语音"))
-async def send_voice(bot:HoshinoBot,ev:CQEvent):
+    await game.check_answer(msg, ev.user_id)
+
+
+@sv.on_prefix(("崩坏3语音", "崩坏语音", "bh3语音", "bh语音", "崩3语音", "崩三语音"))
+async def send_voice(bot: HoshinoBot, ev: CQEvent):
     msg = ev.message.extract_plain_text().strip()
     uid = ev.user_id
     a_list = GameSession.__load__("answer.json")
-    assert(isinstance(a_list,dict))
-    for k,v in a_list.items():
+    assert(isinstance(a_list, dict))
+    for k, v in a_list.items():
         if msg in v:
             if not flmt.check(uid):
-                await bot.send(ev,f"{FN}s内只能获取一次语音，{int(flmt.left_time(uid))}s后再试。",at_sender=True)
+                await bot.send(ev, f"{FN}s内只能获取一次语音，{int(flmt.left_time(uid))}s后再试。", at_sender=True)
                 return
             v_list = GameSession.__load__()['normal'][k]
             voice = random.choice(v_list)
             voice_path = f"file:///{os.path.join(os.path.dirname(__file__),'../assets/record',voice['voice_path'])}"
-            await bot.send(ev,MessageSegment.record(voice_path))
+            await bot.send(ev, MessageSegment.record(voice_path))
             flmt.start_cd(uid)
             return
-    await bot.send(ev,f"没找到【{msg}】的语音，请检查输入。",at_sender=True)
+    await bot.send(ev, f"没找到【{msg}】的语音，请检查输入。", at_sender=True)
+
+
 @sv.on_prefix("崩坏3语音新增答案")
-async def add_answer(bot:HoshinoBot,ev:CQEvent):
+async def add_answer(bot: HoshinoBot, ev: CQEvent):
     msg = ev.message.extract_plain_text().strip()
     try:
-        origin,new = re.split(r":|：",msg)
+        origin, new = re.split(r":|：", msg)
     except Exception:
         return
     data = GameSession.__load__("answer.json")
@@ -96,21 +108,23 @@ async def add_answer(bot:HoshinoBot,ev:CQEvent):
     if new in data[origin]:
         return
     data[origin].append(new)
-    with open(os.path.join(os.path.dirname(__file__),"answer.json"),'w',encoding='utf8') as f:
-        json.dump(data,f,ensure_ascii=False,indent=4)
-    await bot.send(ev,'done')
+    with open(os.path.join(os.path.dirname(__file__), "answer.json"), 'w', encoding='utf8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    await bot.send(ev, 'done')
+
+
 @sv.on_fullmatch("更新崩坏3语音列表")
-async def update_voice_list(bot:HoshinoBot,ev:CQEvent):
-    if not priv.check_priv(ev,priv.SU):
+async def update_voice_list(bot: HoshinoBot, ev: CQEvent):
+    if not priv.check_priv(ev, priv.SU):
         return
     data = gen_voice_list()
     data_dict = split_voice_by_chara(data)
-    with open(os.path.join(os.path.dirname(__file__),"record.json"),'w',encoding='utf8') as f:
-        json.dump(data_dict,f,indent=4,ensure_ascii=False)
+    with open(os.path.join(os.path.dirname(__file__), "record.json"), 'w', encoding='utf8') as f:
+        json.dump(data_dict, f, indent=4, ensure_ascii=False)
     num_normal = sum(len(data_dict["normal"][v]) for v in data_dict["normal"])
     num_hard = sum(len(data_dict["hard"][v]) for v in data_dict["hard"])
-    await bot.send(ev,f"崩坏3语音列表更新完成，当前共有语音{num_hard+num_normal}条，其中普通{num_normal}条，困难{num_hard}条")
+    await bot.send(ev, f"崩坏3语音列表更新完成，当前共有语音{num_hard+num_normal}条，其中普通{num_normal}条，困难{num_hard}条")
 if __name__ == '__main__':
     data = gen_voice_list()
-    with open(os.path.join(os.path.dirname(__file__),"record.json"),'w',encoding='utf8') as f:
-        json.dump(split_voice_by_chara(data),f,indent=4,ensure_ascii=False)
+    with open(os.path.join(os.path.dirname(__file__), "record.json"), 'w', encoding='utf8') as f:
+        json.dump(split_voice_by_chara(data), f, indent=4, ensure_ascii=False)
--- guess_voice/game.py
+++ guess_voice/game.py
@@ -1,11 +1,12 @@
 import json
 import os
 import random
-from datetime import datetime,timedelta
-from loguru import logger
-from nonebot import scheduler
+from datetime import datetime, timedelta
+
 from apscheduler.triggers.date import DateTrigger
-from hoshino import MessageSegment,get_bot
+from hoshino import MessageSegment, get_bot
+from nonebot import scheduler
+
 # from apscheduler.schedulers.asyncio import AsyncIOScheduler
 # scheduler = AsyncIOScheduler()
 game_record = {}
@@ -13,51 +14,58 @@ game_record = {}
 
 class GameSession():
     @staticmethod
-    def __load__(file:str="record.json"):
-        with open(os.path.join(os.path.dirname(__file__),file),'r',encoding='utf8') as li:
+    def __load__(file: str = "record.json"):
+        with open(os.path.join(os.path.dirname(__file__), file), 'r', encoding='utf8') as li:
             data = json.load(li)
         return data
-    def __init__(self,gid:int) -> None:
+
+    def __init__(self, gid: int) -> None:
         self.group_id = gid
-        self.record = game_record.get(self.group_id,{})
+        self.record = game_record.get(self.group_id, {})
         self.job_id = f"{self.group_id}_bh3_guess_voice"
         self.voice_list = self.__load__()
-    async def start(self,duration:int=30,difficulty:str="normal"):
+
+    async def start(self, duration: int = 30, difficulty: str = "normal"):
         """difficulty:  normal|hard"""
         if self.record != {}:
             return f"游戏正在进行中"
         self.begin = datetime.now()
         self.end = self.begin + timedelta(seconds=duration)
-        self.chara,vlist = random.choice(list(self.voice_list[difficulty].items()))
+        self.chara, vlist = random.choice(
+            list(self.voice_list[difficulty].items()))
         self.voice = random.choice(vlist)
         game_record.update({
-            self.group_id:{
-                "chara":self.chara,
-                "voice":self.voice,
-                "ok":-1
+            self.group_id: {
+                "chara": self.chara,
+                "voice": self.voice,
+                "ok": -1
             }
         })
         if scheduler.get_job(job_id=self.job_id):
             scheduler.remove_job(self.job_id)
-        scheduler.add_job(self.stop,trigger=DateTrigger(self.end),id=self.job_id,misfire_grace_time=60,coalesce=True,max_instances=1)
+        scheduler.add_job(self.stop, trigger=DateTrigger(
+            self.end), id=self.job_id, misfire_grace_time=60, coalesce=True, max_instances=1)
         record_path = f"file:///{os.path.join(os.path.dirname(__file__),'../assets/record',self.voice['voice_path'])}"
         print(self.answer)
         bot = get_bot()
-        await bot.send_group_msg(group_id=self.group_id,message=f"即将发送一段崩坏3语音，将在{duration}后公布答案。")
+        await bot.send_group_msg(group_id=self.group_id, message=f"即将发送一段崩坏3语音，将在{duration}后公布答案。")
         return f"{MessageSegment.record(record_path)}"
+
     @property
     def answer(self) -> list:
         self.chara = game_record[self.group_id]["chara"]
-        with open(os.path.join(os.path.dirname(__file__),"answer.json"),'r',encoding='utf8') as f:
+        with open(os.path.join(os.path.dirname(__file__), "answer.json"), 'r', encoding='utf8') as f:
             alist = json.load(f)
         return alist[self.chara]
+
     @property
     def is_start(self):
-        self.record = game_record.get(self.group_id,{})
+        self.record = game_record.get(self.group_id, {})
         if self.record == {}:
             return False
         else:
             return True
+
     async def stop(self):
         self.record = game_record.get(self.group_id)
         ok_player = self.record['ok']
@@ -67,9 +75,10 @@ class GameSession():
             ret_msg = f"回答正确的人：{MessageSegment.at(ok_player)}"
         ret_msg = f"{ret_msg}\n正确答案是：{self.chara}"
         bot = get_bot()
-        await bot.send_group_msg(group_id=self.group_id,message=ret_msg)
+        await bot.send_group_msg(group_id=self.group_id, message=ret_msg)
         game_record[self.group_id] = {}
-    async def check_answer(self,ans:str,qid:int):
+
+    async def check_answer(self, ans: str, qid: int):
         self.record = game_record.get(self.group_id)
         if self.record['ok'] > 0:
             return
@@ -79,4 +88,3 @@ class GameSession():
             scheduler.remove_job(self.job_id)
         game_record[self.group_id]["ok"] = qid
         await self.stop()
-
